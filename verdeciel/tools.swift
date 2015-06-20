dd4a2f2426ef5e56e3b338ef873a3cd1c12ec0a6
Devine Lu Linvega <aliceffekt@gmail.com>
2015-06-20 12:41:23 -0400

    Drawing the capsule wireframe.

--- verdeciel/tools.swift
+++ verdeciel/tools.swift
@@ -7,8 +7,36 @@
 //
 
 import Foundation
+import SceneKit
 
 func degToRad( degrees : Float ) -> Float
 {
 	return ( degrees / 180 * Float(M_PI) )
+}
+
+func lineBetweenNodeA(nodeA: SCNVector3, nodeB: SCNVector3) -> SCNNode {
+	
+	let positions: [Float32] = [nodeA.x, nodeA.y, nodeA.z, nodeB.x, nodeB.y, nodeB.z]
+	let positionData = NSData(bytes: positions, length: sizeof(Float32)*positions.count)
+	let indices: [Int32] = [0, 1]
+	let indexData = NSData(bytes: indices, length: sizeof(Int32) * indices.count)
+	let source = SCNGeometrySource(data: positionData, semantic: SCNGeometrySourceSemanticVertex, vectorCount: indices.count, floatComponents: true, componentsPerVector: 3, bytesPerComponent: sizeof(Float32), dataOffset: 0, dataStride: sizeof(Float32) * 3)
+	let element = SCNGeometryElement(data: indexData, primitiveType: SCNGeometryPrimitiveType.Line, primitiveCount: indices.count, bytesPerIndex: sizeof(Int32))
+	let line = SCNGeometry(sources: [source], elements: [element])
+	line.firstMaterial?.lightingModelName = SCNLightingModelConstant
+	line.firstMaterial?.emission.contents = UIColor.whiteColor()
+	return SCNNode(geometry: line)
+}
+func redLine(nodeA: SCNVector3, nodeB: SCNVector3) -> SCNNode {
+	
+	let positions: [Float32] = [nodeA.x, nodeA.y, nodeA.z, nodeB.x, nodeB.y, nodeB.z]
+	let positionData = NSData(bytes: positions, length: sizeof(Float32)*positions.count)
+	let indices: [Int32] = [0, 1]
+	let indexData = NSData(bytes: indices, length: sizeof(Int32) * indices.count)
+	let source = SCNGeometrySource(data: positionData, semantic: SCNGeometrySourceSemanticVertex, vectorCount: indices.count, floatComponents: true, componentsPerVector: 3, bytesPerComponent: sizeof(Float32), dataOffset: 0, dataStride: sizeof(Float32) * 3)
+	let element = SCNGeometryElement(data: indexData, primitiveType: SCNGeometryPrimitiveType.Line, primitiveCount: indices.count, bytesPerIndex: sizeof(Int32))
+	let line = SCNGeometry(sources: [source], elements: [element])
+	line.firstMaterial?.lightingModelName = SCNLightingModelConstant
+	line.firstMaterial?.emission.contents = UIColor.redColor()
+	return SCNNode(geometry: line)
 }
--- verdeciel/world.swift
+++ verdeciel/world.swift
@@ -14,13 +14,134 @@ extension GameViewController
 {
 	func worldSetup()
 	{
-		
 		sceneSetup()
-		capsuleSetup()
 		objectSetup()
+		createCapsule()
 		sceneComplete()
+	}
+	
+	func createCapsule()
+	{
+		NSLog("WORLD  | Setup")
 		
-		doorClose()
+		var scale:Float = 0.25
+		var height:Float = -2
+		
+		let floor1 = SCNVector3(x: 2 * scale, y: height, z: -4 * scale)
+		let floor2 = SCNVector3(x: 4 * scale, y: height, z: -2 * scale)
+		let floor3 = SCNVector3(x: 4 * scale, y: height, z: 2 * scale)
+		let floor4 = SCNVector3(x: 2 * scale, y: height, z: 4 * scale)
+		let floor5 = SCNVector3(x: -2 * scale, y: height, z: 4 * scale)
+		let floor6 = SCNVector3(x: -4 * scale, y: height, z: 2 * scale)
+		let floor7 = SCNVector3(x: -4 * scale, y: height, z: -2 * scale)
+		let floor8 = SCNVector3(x: -2 * scale, y: height, z: -4 * scale)
+		
+		scene.rootNode.addChildNode(lineBetweenNodeA(floor1,floor2))
+		scene.rootNode.addChildNode(lineBetweenNodeA(floor2,floor3))
+		scene.rootNode.addChildNode(lineBetweenNodeA(floor3,floor4))
+		scene.rootNode.addChildNode(lineBetweenNodeA(floor4,floor5))
+		scene.rootNode.addChildNode(lineBetweenNodeA(floor5,floor6))
+		scene.rootNode.addChildNode(lineBetweenNodeA(floor6,floor7))
+		scene.rootNode.addChildNode(lineBetweenNodeA(floor7,floor8))
+		scene.rootNode.addChildNode(lineBetweenNodeA(floor8,floor1))
+		
+		height = 2
+		
+		let ceiling1 = SCNVector3(x: 2 * scale, y: height, z: -4 * scale)
+		let ceiling2 = SCNVector3(x: 4 * scale, y: height, z: -2 * scale)
+		let ceiling3 = SCNVector3(x: 4 * scale, y: height, z: 2 * scale)
+		let ceiling4 = SCNVector3(x: 2 * scale, y: height, z: 4 * scale)
+		let ceiling5 = SCNVector3(x: -2 * scale, y: height, z: 4 * scale)
+		let ceiling6 = SCNVector3(x: -4 * scale, y: height, z: 2 * scale)
+		let ceiling7 = SCNVector3(x: -4 * scale, y: height, z: -2 * scale)
+		let ceiling8 = SCNVector3(x: -2 * scale, y: height, z: -4 * scale)
+		
+		scene.rootNode.addChildNode(lineBetweenNodeA(ceiling1,ceiling2))
+		scene.rootNode.addChildNode(lineBetweenNodeA(ceiling2,ceiling3))
+		scene.rootNode.addChildNode(lineBetweenNodeA(ceiling3,ceiling4))
+		scene.rootNode.addChildNode(lineBetweenNodeA(ceiling4,ceiling5))
+		scene.rootNode.addChildNode(lineBetweenNodeA(ceiling5,ceiling6))
+		scene.rootNode.addChildNode(lineBetweenNodeA(ceiling6,ceiling7))
+		scene.rootNode.addChildNode(lineBetweenNodeA(ceiling7,ceiling8))
+		scene.rootNode.addChildNode(lineBetweenNodeA(ceiling8,ceiling1))
+		
+		scale = 1
+		height = -1.5
+		
+		let lowRing1 = SCNVector3(x: 2 * scale, y: height, z: -4 * scale)
+		let lowRing2 = SCNVector3(x: 4 * scale, y: height, z: -2 * scale)
+		let lowRing3 = SCNVector3(x: 4 * scale, y: height, z: 2 * scale)
+		let lowRing4 = SCNVector3(x: 2 * scale, y: height, z: 4 * scale)
+		let lowRing5 = SCNVector3(x: -2 * scale, y: height, z: 4 * scale)
+		let lowRing6 = SCNVector3(x: -4 * scale, y: height, z: 2 * scale)
+		let lowRing7 = SCNVector3(x: -4 * scale, y: height, z: -2 * scale)
+		let lowRing8 = SCNVector3(x: -2 * scale, y: height, z: -4 * scale)
+		
+		scene.rootNode.addChildNode(lineBetweenNodeA(lowRing1,lowRing2))
+		scene.rootNode.addChildNode(lineBetweenNodeA(lowRing2,lowRing3))
+		scene.rootNode.addChildNode(lineBetweenNodeA(lowRing3,lowRing4))
+		scene.rootNode.addChildNode(lineBetweenNodeA(lowRing4,lowRing5))
+		scene.rootNode.addChildNode(lineBetweenNodeA(lowRing5,lowRing6))
+		scene.rootNode.addChildNode(lineBetweenNodeA(lowRing6,lowRing7))
+		scene.rootNode.addChildNode(lineBetweenNodeA(lowRing7,lowRing8))
+		scene.rootNode.addChildNode(lineBetweenNodeA(lowRing8,lowRing1))
+		
+		scale = 1
+		height = 1.5
+		
+		let highRing1 = SCNVector3(x: 2 * scale, y: height, z: -4 * scale)
+		let highRing2 = SCNVector3(x: 4 * scale, y: height, z: -2 * scale)
+		let highRing3 = SCNVector3(x: 4 * scale, y: height, z: 2 * scale)
+		let highRing4 = SCNVector3(x: 2 * scale, y: height, z: 4 * scale)
+		let highRing5 = SCNVector3(x: -2 * scale, y: height, z: 4 * scale)
+		let highRing6 = SCNVector3(x: -4 * scale, y: height, z: 2 * scale)
+		let highRing7 = SCNVector3(x: -4 * scale, y: height, z: -2 * scale)
+		let highRing8 = SCNVector3(x: -2 * scale, y: height, z: -4 * scale)
+		
+		scene.rootNode.addChildNode(lineBetweenNodeA(highRing1,highRing2))
+		scene.rootNode.addChildNode(lineBetweenNodeA(highRing2,highRing3))
+		scene.rootNode.addChildNode(lineBetweenNodeA(highRing3,highRing4))
+		scene.rootNode.addChildNode(lineBetweenNodeA(highRing4,highRing5))
+		scene.rootNode.addChildNode(lineBetweenNodeA(highRing5,highRing6))
+		scene.rootNode.addChildNode(lineBetweenNodeA(highRing6,highRing7))
+		scene.rootNode.addChildNode(lineBetweenNodeA(highRing7,highRing8))
+		scene.rootNode.addChildNode(lineBetweenNodeA(highRing8,highRing1))
+		
+		
+		
+		scene.rootNode.addChildNode(lineBetweenNodeA(floor1,lowRing1))
+		scene.rootNode.addChildNode(lineBetweenNodeA(lowRing1,highRing1))
+		scene.rootNode.addChildNode(lineBetweenNodeA(highRing1,ceiling1))
+		scene.rootNode.addChildNode(lineBetweenNodeA(floor2,lowRing2))
+		scene.rootNode.addChildNode(lineBetweenNodeA(lowRing2,highRing2))
+		scene.rootNode.addChildNode(lineBetweenNodeA(highRing2,ceiling2))
+		scene.rootNode.addChildNode(lineBetweenNodeA(floor3,lowRing3))
+		scene.rootNode.addChildNode(lineBetweenNodeA(lowRing3,highRing3))
+		scene.rootNode.addChildNode(lineBetweenNodeA(highRing3,ceiling3))
+		scene.rootNode.addChildNode(lineBetweenNodeA(floor4,lowRing4))
+		scene.rootNode.addChildNode(lineBetweenNodeA(lowRing4,highRing4))
+		scene.rootNode.addChildNode(lineBetweenNodeA(highRing4,ceiling4))
+		scene.rootNode.addChildNode(lineBetweenNodeA(floor5,lowRing5))
+		scene.rootNode.addChildNode(lineBetweenNodeA(lowRing5,highRing5))
+		scene.rootNode.addChildNode(lineBetweenNodeA(highRing5,ceiling5))
+		scene.rootNode.addChildNode(lineBetweenNodeA(floor6,lowRing6))
+		scene.rootNode.addChildNode(lineBetweenNodeA(lowRing6,highRing6))
+		scene.rootNode.addChildNode(lineBetweenNodeA(highRing6,ceiling6))
+		scene.rootNode.addChildNode(lineBetweenNodeA(floor7,lowRing7))
+		scene.rootNode.addChildNode(lineBetweenNodeA(lowRing7,highRing7))
+		scene.rootNode.addChildNode(lineBetweenNodeA(highRing7,ceiling7))
+		scene.rootNode.addChildNode(lineBetweenNodeA(floor8,lowRing8))
+		scene.rootNode.addChildNode(lineBetweenNodeA(lowRing8,highRing8))
+		scene.rootNode.addChildNode(lineBetweenNodeA(highRing8,ceiling8))
+		
+		
+		scene.rootNode.addChildNode(redLine(lowRing1,highRing8))
+		scene.rootNode.addChildNode(redLine(lowRing8,highRing1))
+		
+		
+		
+		
+		
 	}
 	
 	func sceneSetup()
@@ -48,65 +169,23 @@ extension GameViewController
 		scene.rootNode.addChildNode(ambientLightNode)
 	}
 	
-	func capsuleSetup()
-	{
-		NSLog("CAPSUL | Setup")
-		
-		let meshLibrary = SCNScene(named: "art.scnassets/source.dae")
-	
-		let mainCapsule = meshLibrary?.rootNode.childNodeWithName("mainCapsule", recursively: true)!
-		mainCapsule?.scale = SCNVector3(x: scaleValue, y: scaleValue, z: scaleValue)
-		mainCapsule?.position = SCNVector3(x: 0, y: 0, z: 0)
-		scene.rootNode.addChildNode(mainCapsule!)
-		
-		let radarCapsule = meshLibrary?.rootNode.childNodeWithName("radarCapsule", recursively: true)!
-		radarCapsule?.scale = SCNVector3(x: scaleValue, y: scaleValue, z: scaleValue)
-		radarCapsule?.position = SCNVector3(x: 0, y: 0, z: 10)
-		scene.rootNode.addChildNode(radarCapsule!)
-		
-		let comCapsule = meshLibrary?.rootNode.childNodeWithName("comCapsule", recursively: true)!
-		comCapsule?.scale = SCNVector3(x: scaleValue, y: scaleValue, z: scaleValue)
-		comCapsule?.position = SCNVector3(x: 0, y: 0, z: -10)
-		scene.rootNode.addChildNode(comCapsule!)
-		
-		let spaceFar = meshLibrary?.rootNode.childNodeWithName("space", recursively: true)!
-		spaceFar?.scale = SCNVector3(x: 1, y: 1, z: 1)
-		spaceFar?.position = SCNVector3(x: 0, y: 0, z: 0)
-		scene.rootNode.addChildNode(spaceFar!)
-		
-		scene.rootNode.childNodeWithName("space", recursively: true)!.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(0, y: 0, z: 2, duration: 20)))
-	}
-	
 	func objectSetup()
 	{
-		let sphere = SCNSphere(radius: 3)
+		let sphere = SCNSphere(radius: 0.5)
 		let sphereNode = SCNNode(geometry: sphere)
 		sphereNode.name = "trigger.move"
 		sphereNode.position = SCNVector3(x: 0, y: 0, z: 10)
-		sphereNode.opacity = 0.01
 		scene.rootNode.addChildNode(sphereNode)
 		
-		let camCapsuleLinkMesh = SCNSphere(radius: 3)
-		let camCapsuleLink = SCNNode(geometry: camCapsuleLinkMesh)
-		camCapsuleLink.name = "trigger.move"
-		camCapsuleLink.position = SCNVector3(x: 0, y: 0, z: -10)
-		camCapsuleLink.opacity = 0.01
-		scene.rootNode.addChildNode(camCapsuleLink)
-		
-		let sphere3 = SCNSphere(radius: 3)
+		let sphere3 = SCNSphere(radius: 0.5)
 		let sphereNode3 = SCNNode(geometry: sphere3)
 		sphereNode3.name = "trigger.move"
 		sphereNode3.position = SCNVector3(x: 0, y: 0, z: 0)
-		sphereNode3.opacity = 0.01
 		scene.rootNode.addChildNode(sphereNode3)
 	}
 	
 	func sceneComplete()
 	{
-		// Fix textures
-		scene.rootNode.childNodeWithName("door.window.mesh", recursively: true)!.geometry?.firstMaterial?.transparency = 0.1
-		scene.rootNode.childNodeWithName("trigger.power", recursively: true)!.geometry?.firstMaterial?.transparency = 0
-		
 		// retrieve the SCNView
 		let scnView = self.view as! SCNView
 		
