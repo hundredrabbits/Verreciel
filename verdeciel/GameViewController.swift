8db7162c5f3cff9666df28e7ee8ff91d0ff55b41
Devine Lu Linvega <aliceffekt@gmail.com>
2015-07-18 12:03:08 -0400

    Migrated the world settings into the GVC

--- verdeciel/GameViewController.swift
+++ verdeciel/GameViewController.swift
@@ -25,6 +25,50 @@ var cyan:UIColor = UIColor(red: 0.44, green: 0.87, blue: 0.76, alpha: 1)
 var clear:UIColor = UIColor(white: 0, alpha: 0)
 var grey:UIColor = UIColor(white: 0.4, alpha: 1)
 
+var ceilingNode:Array<SCNVector3>!
+var highMidNode:Array<SCNVector3>!
+var highNode:Array<SCNVector3>!
+var lowNode:Array<SCNVector3>!
+var lowMidNode:Array<SCNVector3>!
+var floorNode:Array<SCNVector3>!
+
+var radar:PanelRadar!
+var navigation:PanelNavigation!
+var monitor:PanelMonitor!
+var thruster:PanelThruster!
+var breaker:PanelBreaker!
+var beacon:PanelBeacon!
+var radio:PanelRadio!
+var console:PanelConsole!
+var scanner:PanelScanner!
+
+var capsule:CapsuleNode!
+var player:PlayerNode!
+
+var time:NSTimer!
+
+enum alignment {
+	case left
+	case center
+	case right
+}
+enum cardinals {
+	case n
+	case ne
+	case e
+	case se
+	case s
+	case sw
+	case w
+	case nw
+}
+enum eventTypes {
+	case unknown
+	case station
+	case star
+	case sentry
+}
+
 class GameViewController: UIViewController
 {
     override func viewDidLoad()
@@ -33,6 +77,37 @@ class GameViewController: UIViewController
 		worldSetup()
 		eventSetup()
 	}
+	
+	//
+	
+	func worldSetup()
+	{
+		capsule = CapsuleNode()
+		scene.rootNode.addChildNode(capsule)
+		
+		player = PlayerNode()
+		scene.rootNode.addChildNode(player)
+		
+		sceneComplete()
+	}
+	
+	func sceneComplete()
+	{
+		let scnView = self.view as! SCNView
+		scnView.scene = scene
+		scnView.showsStatistics = false
+		scnView.backgroundColor = UIColor.blackColor()
+		scnView.antialiasingMode = SCNAntialiasingMode.None
+		
+		let gestureRecognizers = NSMutableArray()
+		gestureRecognizers.addObject(UITapGestureRecognizer(target: self, action: "handleTap:"))
+		if let existingGestureRecognizers = scnView.gestureRecognizers {
+			gestureRecognizers.addObjectsFromArray(existingGestureRecognizers)
+		}
+		scnView.gestureRecognizers = gestureRecognizers as [AnyObject]
+	}
+	
+	//
 	
 	override func shouldAutorotate() -> Bool {
 		return true
