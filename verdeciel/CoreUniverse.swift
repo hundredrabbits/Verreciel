e6b73a38d3fcec88c88ab1a563a11bde3feea26a
Devine Lu Linvega <aliceffekt@gmail.com>
2015-09-24 17:13:22 -0400

    Added a teapot

--- verdeciel/CoreUniverse.swift
+++ verdeciel/CoreUniverse.swift
@@ -18,6 +18,6 @@ class CoreUniverse
 		radar.addEvent(SCNEvent(newName: "test", location: CGPoint(x: 0,y: 0.5), size: 0.5, type: eventTypes.location))
 		radar.addEvent(SCNEvent(newName: "star", location: CGPoint(x: 4,y: 4), size: 1, type: eventTypes.location))
 		radar.addEvent(SCNEventNPC(newName: "test", location: CGPoint(x: 6,y: 0.5), size: 1.5,details:"test",note:"nothing"))
-		radar.addEvent(SCNEvent(newName: "item", location: CGPoint(x: -0.5,y: -0.5), size: 1, type: eventTypes.item))
+		radar.addEvent(SCNEvent(newName: "a teapot", location: CGPoint(x: -0.5,y: -0.5), size: 1, type: eventTypes.item, details:"misc"))
 	}
 }
