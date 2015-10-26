eb5715116e5573ff2ac53e87cef3a7ce59d2f774
Devine Lu Linvega <aliceffekt@gmail.com>
2015-10-26 17:33:52 -0400

    Converted handles into SCNNode

--- verdeciel/SCNHandle.swift
+++ verdeciel/SCNHandle.swift
@@ -11,7 +11,7 @@ import QuartzCore
 import SceneKit
 import Foundation
 
-class SCNHandle : Panel
+class SCNHandle : SCNNode
 {
 	var destination:SCNVector3!
 	var selectionLine1:SCNLine!
@@ -19,35 +19,34 @@ class SCNHandle : Panel
 	var selectionLine3:SCNLine!
 	var trigger:SCNTrigger!
 	
+	var isEnabled:Bool = true
+	
 	init(destination:SCNVector3 = SCNVector3())
 	{
 		self.destination = destination
 		
 		super.init()
-	}
-	
-	override func installation()
-	{
 		
+		setup()
 	}
 	
-	override func setup()
+	func setup()
 	{
-		interface.position = SCNVector3(x: 0, y: 0, z: templates.radius)
+		position = SCNVector3(x: 0, y: 0, z: templates.radius)
 		
 		selectionLine1 = SCNLine(nodeA: SCNVector3(x: -0.3, y: 0.1, z: 0),nodeB: SCNVector3(x: 0.3, y: 0.1, z: 0),color:cyan)
 		selectionLine2 = SCNLine(nodeA: SCNVector3(x: -0.3, y: 0, z: 0),nodeB: SCNVector3(x: 0.3, y: 0, z: 0),color:cyan)
 		selectionLine3 = SCNLine(nodeA: SCNVector3(x: -0.3, y: -0.1, z: 0),nodeB: SCNVector3(x: 0.3, y: -0.1, z: 0),color:cyan)
-		interface.addChildNode(selectionLine1)
-		interface.addChildNode(selectionLine2)
-		interface.addChildNode(selectionLine3)
+		addChildNode(selectionLine1)
+		addChildNode(selectionLine2)
+		addChildNode(selectionLine3)
 		
 		trigger = SCNTrigger(host: self, size: CGSize(width: 2, height: 0.5), operation: 0)
 		trigger.updateChildrenColors(red)
-		interface.addChildNode(trigger)
+		addChildNode(trigger)
 	}
 	
-	override func enable()
+	func enable()
 	{
 		isEnabled = true
 		selectionLine1.updateColor(cyan)
@@ -55,7 +54,7 @@ class SCNHandle : Panel
 		selectionLine3.updateColor(cyan)
 	}
 	
-	override func disable()
+	func disable()
 	{
 		isEnabled = false
 		selectionLine1.updateColor(grey)
@@ -71,7 +70,6 @@ class SCNHandle : Panel
 		SCNTransaction.setAnimationDuration(2.5)
 		player.position = destination
 		ui.position = destination
-		SCNTransaction.setCompletionBlock({ })
 		SCNTransaction.commit()
 		
 		player.handle = self
