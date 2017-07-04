ffb4ba0c97ad88b76cc2c1ca764e2ede46272294
Rezmason <jeremysachs@rezmason.net>
2017-07-04 07:02:00 -0700

    Scenewire now has one vertex updating function, so they all animate properly.

--- verreciel_js/scripts/components/scenewire.js
+++ verreciel_js/scripts/components/scenewire.js
@@ -38,10 +38,10 @@ class SceneWire extends Empty
     this.vertex3 = new THREE.Vector3(this.endB.x * 0.6, this.endB.y * 0.6, this.endB.z * 0.6);
     this.vertex4 = new THREE.Vector3(this.endB.x * 0.8, this.endB.y * 0.8, this.endB.z * 0.8);
     
-    this.segment1.updateGeometry( [this.endA, this.vertex1], verreciel.white);
-    this.segment2.updateGeometry( [this.vertex1, this.vertex2], verreciel.red);
-    this.segment3.updateGeometry( [this.vertex2, this.vertex3], verreciel.red);
-    this.segment4.updateGeometry( [this.vertex3, this.vertex4], verreciel.red);
+    this.segment1.updateGeometry( [this.endA, this.vertex1], verreciel.cyan);
+    this.segment2.updateGeometry( [this.vertex1, this.vertex2], verreciel.white);
+    this.segment3.updateGeometry( [this.vertex2, this.vertex3], verreciel.white);
+    this.segment4.updateGeometry( [this.vertex3, this.vertex4], verreciel.white);
     this.segment5.updateGeometry( [this.vertex4, this.endB], verreciel.red);
   }
   
@@ -59,21 +59,7 @@ class SceneWire extends Empty
       return;
     }
     
-    this.vertex1 = new THREE.Vector3(this.endB.x * 0.2, this.endB.y * 0.2, this.endB.z * 0.2);
-    this.vertex2 = new THREE.Vector3(this.endB.x * 0.4, this.endB.y * 0.4, this.endB.z * 0.4);
-    this.vertex3 = new THREE.Vector3(this.endB.x * 0.6, this.endB.y * 0.6, this.endB.z * 0.6);
-    this.vertex4 = new THREE.Vector3(this.endB.x * 0.8, this.endB.y * 0.8, this.endB.z * 0.8);
-    
-    this.vertex1.y += Math.sin((verreciel.game.time + this.vertex1.x + this.vertex1.y + this.vertex1.z)/20) * 0.05;
-    this.vertex2.y += Math.sin((verreciel.game.time + this.vertex2.x + this.vertex2.y + this.vertex2.z)/20) * 0.08;
-    this.vertex3.y += Math.sin((verreciel.game.time + this.vertex3.x + this.vertex3.y + this.vertex3.z)/20) * 0.08;
-    this.vertex4.y += Math.sin((verreciel.game.time + this.vertex4.x + this.vertex4.y + this.vertex4.z)/20) * 0.05;
-    
-    this.segment1.updateGeometry( [this.endA, this.vertex1], verreciel.cyan);
-    this.segment2.updateGeometry( [this.vertex1, this.vertex2], verreciel.white);
-    this.segment3.updateGeometry( [this.vertex2, this.vertex3], verreciel.white);
-    this.segment4.updateGeometry( [this.vertex3, this.vertex4], verreciel.white);
-    this.segment5.updateGeometry( [this.vertex4, this.endB], verreciel.red);
+    this.updateSegments();
   }
   
   updateEnds(endA = new THREE.Vector3(), endB = new THREE.Vector3())
@@ -87,11 +73,21 @@ class SceneWire extends Empty
     this.vertex3 = new THREE.Vector3(this.endB.x * 0.6, this.endB.y * 0.6, this.endB.z * 0.6);
     this.vertex4 = new THREE.Vector3(this.endB.x * 0.8, this.endB.y * 0.8, this.endB.z * 0.8);
     
-    this.segment1.updateGeometry( [this.endA, this.vertex1], verreciel.white);
-    this.segment2.updateGeometry( [this.vertex1, this.vertex2], verreciel.red);
-    this.segment3.updateGeometry( [this.vertex2, this.vertex3], verreciel.red);
-    this.segment4.updateGeometry( [this.vertex3, this.vertex4], verreciel.red);
-    this.segment5.updateGeometry( [this.vertex4, this.endB], verreciel.red);
+    this.updateSegments();
+  }
+
+  updateSegments()
+  {
+    this.vertex1.y = this.endB.y * 0.2 + Math.sin((verreciel.game.time + this.vertex1.x + this.vertex1.y + this.vertex1.z)/20) * 0.05;
+    this.vertex2.y = this.endB.y * 0.4 + Math.sin((verreciel.game.time + this.vertex2.x + this.vertex2.y + this.vertex2.z)/20) * 0.08;
+    this.vertex3.y = this.endB.y * 0.6 + Math.sin((verreciel.game.time + this.vertex3.x + this.vertex3.y + this.vertex3.z)/20) * 0.08;
+    this.vertex4.y = this.endB.y * 0.8 + Math.sin((verreciel.game.time + this.vertex4.x + this.vertex4.y + this.vertex4.z)/20) * 0.05;
+    
+    this.segment1.updateGeometry( [this.endA, this.vertex1], this.segment1.color);
+    this.segment2.updateGeometry( [this.vertex1, this.vertex2], this.segment2.color);
+    this.segment3.updateGeometry( [this.vertex2, this.vertex3], this.segment3.color);
+    this.segment4.updateGeometry( [this.vertex3, this.vertex4], this.segment4.color);
+    this.segment5.updateGeometry( [this.vertex4, this.endB], this.segment5.color);
   }
   
   enable()
@@ -106,15 +102,6 @@ class SceneWire extends Empty
     assertArgs(arguments, 0);
     this.isEnabled = false;
     this.hide();
-  }
-  
-  blink()
-  {
-    assertArgs(arguments, 0);
-    for (let node of this.children)
-    {
-      node.hide();
-    }
   }
   
   isCompatible()
