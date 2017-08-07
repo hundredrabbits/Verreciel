b0c51e0d94cc0ffbab9a50e57f77a0f958bc68cc
Rezmason <jeremysachs@rezmason.net>
2017-08-06 18:38:34 -0700

    Knew I forgot something. >_<

--- verreciel_js/scripts/meshlinesegments.js
+++ verreciel_js/scripts/meshlinesegments.js
@@ -1,3 +1,6 @@
+//  Created by Devine Lu Linvega.
+//  Copyright © 2017 XXIIVV. All rights reserved.
+
 class MeshLineSegments {
   constructor(source = null) {
     this.geometry = new THREE.BufferGeometry();
